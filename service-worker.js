d9764cd1b8ad2b6a72f968fb86d48c984609e6e4
Zelalem Eshetu <zelalemeshetuhabtu@gmail.com>
2026-06-29 18:00:03 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// መረጃዎችን ስልክ ላይ ለመጫን (Install)
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// ያለ ኢንተርኔት ሲሆን መረጃዎችን ከስልክ ለማንበብ (Fetch)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
